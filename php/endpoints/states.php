13ba7abb1a7510a0cb1867b07add84d7b88a4d2e
thevictorgreen <thevictorgreen@gmail.com>
2017-12-11 11:54:57 -0500

    slim framework

---
+++ php/endpoints/states.php
@@ -0,0 +1,26 @@
+<?php
+
+  use \Psr\Http\Message\ServerRequestInterface as Request;
+  use \Psr\Http\Message\ResponseInterface as Response;
+
+
+  function states( Request $request, Response $response ) {
+
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Origin: *");
+
+    // DATABASE CONNECTION CODE
+    $db_host = getenv('DB_HOST');
+    $db_name = getenv('DB_NAME');
+    $db_user = getenv('DB_USER');
+    $db_pass = getenv('DB_PASS');
+    $db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8mb4',$db_user,$db_pass);
+    // DATABASE CONNECTION CODE
+
+    $results = $db->query("Select * from states");
+    echo json_encode( $results->fetchAll(PDO::FETCH_ASSOC)) ;
+
+    exit;
+  }
+
+?>
