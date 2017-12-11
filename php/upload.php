0789daa42153618cb17cf1682821da346f1155cf
thevictorgreen <thevictorgreen@gmail.com>
2017-12-11 13:36:55 -0500

    upload 3

--- php/upload.php
+++ php/upload.php
@@ -5,14 +5,14 @@
  if(isset($_FILES["myfile"])) {
 
 	//Filter the file types , if you want.
-	if ($_FILES["myfile"]["error"] > 0) {
+	/*if ($_FILES["myfile"]["error"] > 0) {
     echo "Error: " . $_FILES["file"]["error"] . "<br>";
 	}
 	else {
     //move the uploaded file to uploads folder;
     move_uploaded_file($_FILES["myfile"]["tmp_name"],$output_dir. $_FILES["myfile"]["name"]);
    	echo "Uploaded File :".$_FILES["myfile"]["name"];
-	}
+	}*/
  }
 
 ?>
