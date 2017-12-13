8c33af3b79683cba6d47483dc4295e1799c1725a
thevictorgreen <thevictorgreen@gmail.com>
2017-12-13 13:37:46 -0500

    addjacker

---
+++ php/endpoints/addjacker.php
@@ -0,0 +1,18 @@
+<?php
+
+ use \Psr\Http\Message\ServerRequestInterface as Request;
+ use \Psr\Http\Message\ResponseInterface as Response;
+
+
+ function addjacker( Request $request, Response $response ) {
+
+   header('Content-type: application/json');
+   header('Access-Control-Allow-Origin: *');
+
+   echo json_encode( $request->getParsedBody()['book_name'] );
+
+   exit;
+
+ }
+
+?>
