9b55c09d6c16d7abbdf1506b7df60d554ff413df
thevictorgreen <thevictorgreen@gmail.com>
2017-12-14 12:52:28 -0500

    addjacker

--- php/endpoints/addjacker.php
+++ php/endpoints/addjacker.php
@@ -3,17 +3,29 @@
  use \Psr\Http\Message\ServerRequestInterface as Request;
  use \Psr\Http\Message\ResponseInterface as Response;
 
+ class Jacker {
+   function Jacker() {
+     $this->image = "";
+     $this->imageId = "";
+     $this->firstName = "";
+   }
+ }
+
 
  function addjacker( Request $request, Response $response ) {
 
    header('Content-type: application/json');
    header('Access-Control-Allow-Origin: *');
 
-   $image = $request->getParsedBody()['image'];
+   $jacker = new Jacker();
+
+   $jacker.image = $request->getParsedBody()['image'];
+   $jacker.imageId = $request->getParsedBody()['imageId'];
+   $jacker.firstName = $request->getParsedBody()['firstName'];
 
    $hex = shell_exec('./endpoints/utils/convertJson2Hex.sh');
 
-   $response->getBody()->write( json_encode( $image ) );
+   $response->getBody()->write( json_encode( $jacker ) );
 
  }
 
