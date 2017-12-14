c147190f3dd1d0cd072b267f97429011d566ee27
thevictorgreen <thevictorgreen@gmail.com>
2017-12-14 14:24:15 -0500

    addjacker

--- php/endpoints/addjacker.php
+++ php/endpoints/addjacker.php
@@ -52,8 +52,35 @@
      $jacker_hex .= $line;
    }
 
-   $response->getBody()->write( json_encode( $jacker_hex ) );
+   //$response->getBody()->write( json_encode( $jacker_hex ) );
+   $response->getBody()->write( json_encode( saveJacker() ) );
 
+ }
+
+
+ function saveJacker() {
+
+   $service_url = 'http://blockchain:8000';
+   $credentials = "privateblock:password";
+
+   $curl = curl_init();
+   curl_setopt( $curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC );
+   curl_setopt( $curl, CURLOPT_USERPWD, $credentials );
+   curl_setopt( $curl, CURLOPT_URL, $service_url );
+   curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+   curl_setopt( $curl, CURLOPT_POST, true );
+   curl_setopt( $curl, CURLOPT_POSTFIELDS, '{"method":"getinfo","params":[],"id":1,"chain_name":"Skynet"}' );
+   curl_setopt( $curl, CURLOPT_HTTPHEADER, array('Content-Type: text/plain') );
+   $curl_response = curl_exec($curl);
+
+   if ($curl_response === false) {
+       $info = curl_getinfo($curl);
+       curl_close($curl);
+       die('error occured during curl exec. Additioanl info: ' . var_export($info));
+   }
+
+   curl_close($curl);
+   return $curl_response;
  }
 
 ?>
